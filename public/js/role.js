ffcef304fc4f6c10a1120e75b77b7041dfb14eb0
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-05-02 14:30:49 +0100

    added code to handle the saveRole button (POST request to /api/savedRole)

---
+++ public/js/role.js
@@ -0,0 +1,30 @@
+const saveBtnHandler = async (event) => {
+    
+  
+    if (event.target.hasAttribute('data-id')) {      //if the thing clicked has the "data-id" attribute then ..:
+        
+        const role_id = event.target.getAttribute('data-id');
+    
+        const response = await fetch(`/api/savedRole/`, {
+        method: 'POST',
+        body: JSON.stringify({ role_id }),
+        headers: { 'Content-Type': 'application/json' },
+        });
+        console.log(response)
+    
+        if (response.ok) {
+             // changes the button text to 'Saved'
+             event.target.textContent = 'Saved';
+             // disables the button after saving
+             event.target.disabled = true;
+        } else {
+        alert('Failed to save role');
+        }
+    }
+
+  
+  };
+
+  document
+    .querySelector('.selected-role-card')
+    .addEventListener('click', saveBtnHandler);
