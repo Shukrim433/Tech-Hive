4451f31be13742b1e673763626dcfe09ffe2106f
eji_a <156595423+e-aji@users.noreply.github.com>
2024-05-02 21:36:47 +0100

    fixed seeding and added password input to login

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -1,98 +1,98 @@
-const { Tag } = require('../models');
+// const { Tag } = require('../models');
 
-const tagData = [
-  {
-    tag_name: 'Full Stack',
-  },
-  {
-    tag_name: 'Back End',
-  },
-  {
-    tag_name: 'Front End',
-  },
-  {
-    tag_name: 'HTML5',
-  },
-  {
-    tag_name: 'ReactJS',
-  },
-  {
-    tag_name: 'CSS3',
-  },
-  {
-    tag_name: 'JavaScript',
-  },
-  {
-    tag_name: 'Python',
-  },
-  {
-    tag_name: 'Ruby on Rails',
-  },
-  {
-    tag_name: 'MERNStack',
-  },
-  {
-    tag_name: 'SQL',
-  },
-  {
-    tag_name: 'Django',
-  },
-  {
-    tag_name: 'Vue.js',
-  },
-  {
-    tag_name: 'Angular',
-  },
-  {
-    tag_name: 'Node.js',
-  },
-  {
-    tag_name: 'Express.js',
-  },
-  {
-    tag_name: 'MongoDB',
-  },
-  {
-    tag_name: 'PostgreSQL',
-  },
-  {
-    tag_name: 'RESTful API',
-  },
-  {
-    tag_name: 'GraphQL',
-  },
-  {
-    tag_name: 'Bootstrap',
-  },
-  {
-    tag_name: 'SASS/SCSS',
-  },
-  {
-    tag_name: 'TypeScript',
-  },
-  {
-    tag_name: 'Laravel',
-  },
-  {
-    tag_name: 'PHP',
-  },
-  {
-    tag_name: 'WordPress',
-  },
-  {
-    tag_name: 'JQuery',
-  },
-  {
-    tag_name: 'AWS',
-  },
-  {
-    tag_name: 'Docker',
-  },
-  {
-    tag_name: 'Kubernetes',
-  },
-];
+// const tagData = [
+//   {
+//     tag_name: 'Full Stack',
+//   },
+//   {
+//     tag_name: 'Back End',
+//   },
+//   {
+//     tag_name: 'Front End',
+//   },
+//   {
+//     tag_name: 'HTML5',
+//   },
+//   {
+//     tag_name: 'ReactJS',
+//   },
+//   {
+//     tag_name: 'CSS3',
+//   },
+//   {
+//     tag_name: 'JavaScript',
+//   },
+//   {
+//     tag_name: 'Python',
+//   },
+//   {
+//     tag_name: 'Ruby on Rails',
+//   },
+//   {
+//     tag_name: 'MERNStack',
+//   },
+//   {
+//     tag_name: 'SQL',
+//   },
+//   {
+//     tag_name: 'Django',
+//   },
+//   {
+//     tag_name: 'Vue.js',
+//   },
+//   {
+//     tag_name: 'Angular',
+//   },
+//   {
+//     tag_name: 'Node.js',
+//   },
+//   {
+//     tag_name: 'Express.js',
+//   },
+//   {
+//     tag_name: 'MongoDB',
+//   },
+//   {
+//     tag_name: 'PostgreSQL',
+//   },
+//   {
+//     tag_name: 'RESTful API',
+//   },
+//   {
+//     tag_name: 'GraphQL',
+//   },
+//   {
+//     tag_name: 'Bootstrap',
+//   },
+//   {
+//     tag_name: 'SASS/SCSS',
+//   },
+//   {
+//     tag_name: 'TypeScript',
+//   },
+//   {
+//     tag_name: 'Laravel',
+//   },
+//   {
+//     tag_name: 'PHP',
+//   },
+//   {
+//     tag_name: 'WordPress',
+//   },
+//   {
+//     tag_name: 'JQuery',
+//   },
+//   {
+//     tag_name: 'AWS',
+//   },
+//   {
+//     tag_name: 'Docker',
+//   },
+//   {
+//     tag_name: 'Kubernetes',
+//   },
+// ];
 
-const seedTags = () => Tag.bulkCreate(tagData);
+// const seedTags = () => Tag.bulkCreate(tagData);
 
-module.exports = seedTags;
+// module.exports = seedTags;
