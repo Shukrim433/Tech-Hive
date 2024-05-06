2b766eb52392f6767db5576e7b21a63d87c4cf0b
eji_a <156595423+e-aji@users.noreply.github.com>
2024-05-06 14:22:53 +0100

    made some changes to the role containers and Added helper function to allow for a comma to be put in the salary section

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,6 @@
+const Handlebars = require('handlebars');
 
+// Define a helper function to format numbers with commas
+Handlebars.registerHelper('formatNumberWithCommas', function(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+});
